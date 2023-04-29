28b236a36f12fd378ac6d7fbeaab12caa1a95de7
CoderEx24 <coderex24@protonmail.com>
2023-04-29 19:25:26 +0300

    minor progress on parsing

--- src/propositional_logic/compound_proposition.rs
+++ src/propositional_logic/compound_proposition.rs
@@ -1,6 +1,8 @@
 use super::proposition::Proposition;
 use std::fmt::Display;
+use regex::Regex;
 
+#[derive(Clone, PartialEq)]
 pub enum Operation {
     AND,
     OR,
@@ -9,6 +11,7 @@ pub enum Operation {
     IFF,
 }
 
+#[derive(Clone, PartialEq)]
 pub enum Operands {
     Simple(Proposition, Proposition),
     Complex(Box<CompoundProposition>, Box<CompoundProposition>),
@@ -31,16 +34,41 @@ fn parse_operands(operands: &Operands) -> (String, String) {
     }
 }
 
+#[derive(Clone, PartialEq)]
 pub struct CompoundProposition {
     operands: Operands,
     operation: Operation,
 }
 
 impl CompoundProposition {
-    pub fn new(operands: Operands, operation: Operation) -> CompoundProposition {
+    pub fn new(operands: &Operands, operation: Operation) -> CompoundProposition {
         CompoundProposition {
-            operands,
+            operands: operands.clone(),
             operation,
+        }
+    }
+
+    pub fn parse_sentence(sentence: &str) -> CompoundProposition {
+        let sentence = sentence.to_lowercase();
+        let sentence = sentence.as_str();
+        let and_regex = Regex::new(r"(?P<op1>.+) and (?P<op2>.+)").unwrap();
+
+        let and_captures = and_regex.captures(sentence);
+
+        if and_captures.is_some() {
+            let captures = and_captures.unwrap();
+            let op1 = Proposition::new(&String::from(&captures["op1"]));
+            let op2 = Proposition::new(&String::from(&captures["op2"]));
+
+            let operands = Operands::Simple(op1, op2);
+            return CompoundProposition::new(&operands, Operation::AND);
+
+        } else {
+            let op1 = Proposition::new(&String::from("lol"));
+            let op2 = Proposition::new(&String::from("non"));
+
+            let operands = Operands::Simple(op1, op2);
+            return CompoundProposition::new(&operands, Operation::AND);
         }
     }
 }
