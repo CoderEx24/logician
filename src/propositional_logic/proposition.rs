a296b13856044dd61fba83387055e359e751359e
CoderEx24 <coderex24@protonmail.com>
2023-04-29 16:37:27 +0300

    added Proposition

---
+++ src/propositional_logic/proposition.rs
@@ -0,0 +1,31 @@
+use std::fmt::Display;
+
+pub struct Proposition {
+    text: String,
+    letter: char,
+    negated: bool
+
+}
+
+impl Proposition {
+    pub fn new(s: &String) -> Proposition {
+        Proposition {
+            text: s.clone(),
+            // TODO: better way to assign values to letter and negated
+            letter: s.as_bytes()[0].into(),
+            negated: s.contains("not"),
+        }
+    }
+
+    pub fn negate(&mut self) {
+        self.negated = !self.negated;
+    }
+
+}
+
+impl Display for Proposition {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}: {}", self.letter, self.text)
+    }
+}
+
