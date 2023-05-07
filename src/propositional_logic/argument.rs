f730696c189fb75667db3a2086fca984d3c4579d
CoderEx24 <coderex24@protonmail.com>
2023-05-07 06:29:27 +0300

    implemented something like modes ponens

--- src/propositional_logic/argument.rs
+++ src/propositional_logic/argument.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use std::fmt::Display;
-use super::compound_proposition::{CompoundProposition, Operands};
+use super::compound_proposition::{CompoundProposition, Operands, Operation};
 use super::proposition::Proposition;
 
 pub struct Argument {
@@ -38,8 +38,54 @@ impl Argument {
             conclusion,
             propositional_variables: premises.iter()
                 .map(|p| get_propositional_variables(&p))
-                .reduce(|a, v| a.into_iter().chain(v).collect()).unwrap()
+                .reduce(|a, v| a.into_iter().chain(v).collect()).unwrap(),
         }
+    }
+
+    pub fn check_argument(&self) -> bool {
+        let rules_of_inference = vec![
+            // modes ponens
+            |proposition1: &CompoundProposition, proposition2: &CompoundProposition| -> Option<CompoundProposition> {
+                let (op1, op2) = match proposition1.operands() {
+                    Operands::Simple(a, b) => ((CompoundProposition::new_redendant(&a), CompoundProposition::new_redendant(&b))),
+                    Operands::Complex(a, b) => ((*a.clone(), *b.clone())),
+                    Operands::Mixed(a, b, flip) => if flip {
+                        ((CompoundProposition::new_redendant(&b), *a.clone())) 
+                    } else {
+                        ((*a.clone(), CompoundProposition::new_redendant(&b))) 
+                    }
+                };
+                
+                if proposition1.operation() == Operation::IMPLY && op1 == *proposition2 { 
+                    Some(op2)
+                } else {
+                    None
+                }
+            },
+        ];
+
+        let mut premises = self.premises.clone();
+
+        for op1 in &self.premises {
+            for op2 in &self.premises {
+                let results: Vec<CompoundProposition> = rules_of_inference.iter()
+                    .map(|rule| rule(&op1, &op2))
+                    .filter(|value| value.is_some())
+                    .map(|value| value.unwrap())
+                    .collect();
+
+                if results.len() > 0 {
+                    println!("{} and {} result in the following using modes ponens:-", op1, op2);
+                    for r in &results {
+                        println!("\t{}", r);
+                    }
+                }
+
+            }
+        }
+
+        false
+
     }
 
     pub fn parse_argument(argument: &str) -> Argument {
