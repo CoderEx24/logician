1f03924b7c3f56ff220309407f9fcd14af351181
CoderEx24 <coderex24@protonmail.com>
2023-05-08 20:57:55 +0300

    added simple main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
 mod propositional_logic;
 
+use std::io;
+use propositional_logic::Argument;
+
 fn main() {
-    println!("Hello, world!");
+    let mut line = String::new();
+    let mut argument = String::new();
+
+    loop {
+
+        line.clear();
+
+        println!(" > \r");
+        io::stdin().read_line(&mut line);
+
+        if line == "STOP\n" {
+            break;
+        }
+
+        argument += line.as_str();
+        argument += "\n";
+        
+    }
+
+    let argument = Argument::parse_argument(argument.as_str());
+    println!("\n\n-----------------------------------\n{}", argument);
+
+    argument.check_argument();
 }
